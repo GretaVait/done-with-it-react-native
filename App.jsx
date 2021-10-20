e8a33fbb7a41c5cbc1f8b8444ceff598ee912ff9
GretaVait895 <gretavait@gmail.com>
2021-10-20 09:07:51 +0300

    app context added

--- App.jsx
+++ App.jsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppProvider} from './context/AppContext'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </AppProvider>
   );
 }
 
---
+++ context/AppContext.jsx
@@ -0,0 +1,22 @@
+import React, { useState, createContext } from 'react'
+
+export const AppContext = createContext()
+
+export const AppProvider = (props) => {
+
+  const [userData, setUserData] = useState({
+    origin: null,
+    destination: null,
+    travelTime: 0
+  })
+
+  const saveUserData = (values) => {
+    setUserData(values)
+  }
+
+  return (
+    <AppContext.Provider value={{ userData, saveUserData }}>
+      {props.children}
+    </AppContext.Provider>
+  )
+}
