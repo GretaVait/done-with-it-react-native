5ffe0cafcff8299bfefea525834d26d4f3a1c82f
GretaVait895 <gretavait@gmail.com>
2021-10-22 10:18:35 +0300

    nav favourites functionality

--- components/NavFavourites.jsx
+++ components/NavFavourites.jsx
@@ -1,24 +1,34 @@
 // Base
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 // Lib
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from 'react-native-elements'
+// Context
+import { AppContext } from '../context/AppContext'
+// Navigation
+import { useNavigation } from '@react-navigation/core'
 
-const NavFavourites = () => { 
+const NavFavourites = ({ origin }) => { 
+  const { saveUserData, userData } = useContext(AppContext)
+  const nav = useNavigation()
 
   const data = [
     {
       id: "159",
       icon: "home",
       location: "Home",
-      destination: "Code Street, London, UK"
+      destination: "Code Street, London, UK",
+      lat: 51.522392,
+      lng: -0.07083420000000001
     },
     {
       id: "456",
       icon: "briefcase",
       location: "Work",
-      destination: "London Eye, London, UK"
+      destination: "London Eye, London, UK",
+      lat: 51.5032973,
+      lng: -0.1195537
     }
   ]
 
@@ -29,8 +39,34 @@ const NavFavourites = () => {
       ItemSeparatorComponent={() => (
         <View style={[tw`bg-gray-400`, { height: 0.5 }]} />
       )}
-      renderItem={({ item: { icon, location, destination } }) => (
-        <TouchableOpacity style={tw`flex-row items-center p-5`}>
+      renderItem={({ item: { icon, location, destination, lat, lng } }) => (
+        <TouchableOpacity style={tw`flex-row items-center p-5`} onPress={() => {
+          if (origin) {
+            saveUserData({
+              ...userData,
+              origin: {
+                description: destination,
+                location: {
+                  lat: lat,
+                  lng: lng
+                }
+              }
+            })
+            nav.navigate('Map')
+          } else {
+            saveUserData({
+              ...userData,
+              destination: {
+                description: destination,
+                location: {
+                  lat: lat,
+                  lng: lng
+                }
+              }
+            })
+            nav.navigate('RideOptionsCard')
+          }
+        }}>
           <Icon 
             style={tw`mr-4 rounded-full bg-gray-300 p-3`}
             name={icon}
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -54,7 +54,7 @@ const HomeScreen = () => {
 
         <NavOptions />
 
-        <NavFavourites />
+        <NavFavourites origin />
       </View>
     </View>
   )
